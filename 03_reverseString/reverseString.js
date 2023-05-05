5f2cf30001e74302128847914c04e5cb0558d9f3
Austin Dean <19595957+AustinDean39@users.noreply.github.com>
2023-05-05 15:29:41 -0400

    Complete exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let originalString = string.split('');
+  let reversedString = [];
+  for (let i = originalString.length; i >= 0; i--) {
+    reversedString.push(originalString[i]);
+  }
+  return reversedString.join('');
 };
 
 // Do not edit below this line
