86d2e3989d46e61f7e4fd0e734535b6584618691
Austin Dean <19595957+AustinDean39@users.noreply.github.com>
2023-05-05 15:22:31 -0400

    Completed exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, repeatNum) {
+  if (repeatNum < 0) { return 'ERROR' }
 
+  let resultString = '';
+  for (let i = 0; i < repeatNum; i++) {
+    resultString += string;
+  }
+  
+  return resultString;
 };
 
 // Do not edit below this line
