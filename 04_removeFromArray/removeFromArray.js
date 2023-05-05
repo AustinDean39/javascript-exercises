5240443bdd2c0e368ae33605fdd9839cc7ceae38
Austin Dean <19595957+AustinDean39@users.noreply.github.com>
2023-05-05 16:38:50 -0400

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...values) {
+  let resultArray = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    const val = inputArray[i];
+    if (!values.includes(val)) { resultArray.push(val); }
+  }
 
+  return resultArray;
 };
 
 // Do not edit below this line
