88460ad437fb22c5db25b278669dad340c551f68
Austin Dean <19595957+AustinDean39@users.noreply.github.com>
2023-05-06 12:07:26 -0400

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degF) {
+  let degC = (degF - 32) * (5 / 9);
+  if (Number.isInteger(degC)) {
+    return degC;
+  } else {
+    degC = degC.toFixed(1);
+    return parseFloat(degC);
+  };
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degC) {
+  let degF = (degC * (9 / 5) + 32);
+  if (Number.isInteger(degF)) {
+    return degF;
+  } else {
+    degF = degF.toFixed(1);
+    return parseFloat(degF);
+  };
 };
 
 // Do not edit below this line
