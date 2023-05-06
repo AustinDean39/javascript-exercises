b7006d0369827fe047a2c8b1b8d7f9b8e30e4984
Austin Dean <19595957+AustinDean39@users.noreply.github.com>
2023-05-06 12:23:14 -0400

    Complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	let sum = 0;
+  numArray.forEach(num => {
+    sum += num;
+  });
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numArray) {
+  let product = 1;
+  numArray.forEach(num => {
+    product *= num;
+  });
 
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) {
+    return 'ERROR';
+  } else if (num === 1 || num === 0) {
+    return 1;
+  }
+
+  let factProduct = 1;
+  for (let i = num; i > 0; i--) {
+    factProduct *= i;
+  }
+
+  return factProduct;
 };
 
 // Do not edit below this line
