a5a08dd3b9e57235b418c589d52b7b59ff982a25
Austin Dean <19595957+AustinDean39@users.noreply.github.com>
2023-05-06 11:40:54 -0400

    Complete exercises 05_sumAll and 06_leapYears

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let smaller, larger;
+  if (typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  }
 
+  if (num1 > num2) {
+    larger = num1;
+    smaller = num2;
+  } else {
+    larger = num2;
+    smaller = num1;
+  }
+
+  let finalSum = 0;
+  for (let i = smaller; i <= larger; i++) {
+    finalSum += i;
+  }
+
+  return finalSum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
